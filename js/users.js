a7c00fda2ae805f4312b7f9494c58a533374caee
Bill Punch <punch.w@gmail.com>
2015-04-17 16:10:37 -0400

    Adding some styling

--- js/users.js
+++ js/users.js
@@ -1,5 +1,7 @@
 var usersArray = [
-	["Bill", "Male", "punch.w@gmail.com"],
-	["Sally", "Female", "test2@test.com"],
-	["Jane", "Female", "test3@test.com"]
+	["Quincy", "Wiggins", "enim@semmollis.ca", "(731) 614-2279"],
+	["Brittany", "Mclean", "ipsum.Phasellus.vitae@tincidunt.org", "(137) 331-4056"],
+	["Hope", "Blevins", "diam.luctus@Etiamvestibulummassa.co.uk", "(652) 218-1267"],
+	["Derek", "Palmer", "augue.eu.tempor@a.com", "(540) 389-7589"],
+	["Mari", "Snider", "Quisque.imperdiet@auctor.org", "(415) 237-5954"]
 ];
